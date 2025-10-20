3cc55dd912f4ba5c0a62ca84412004f8650e0aa4
Chinthaka Weerakkody <chinthakaflags@gmail.com>
2025-10-20 11:34:18 +0530

    added case sensivity

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use minigrep::{search_case_insensitive, search};
 use std::error::Error;
 use std::{env, fs, process};
-use minigrep::search;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -17,8 +17,9 @@ fn main() {
 }
 
 struct Config {
-    query: String,
-    file_path: String,
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -30,14 +31,26 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
 fn run(config: &Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(&config.file_path)?;
 
-    for line in search(&config.query.to_string(), &contents.to_string()) {
+    let result = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in result {
         println!("{line}")
     }
 
